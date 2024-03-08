6c0925dabba45ed9db6d1fd458f3920d9e6c9507
Takeo-PC\Takeo <cguzmanpulido@gmail.com>
2024-03-08 14:29:13 +0100

    added fragments behaviour

--- app/src/main/java/com/example/fitapp/MainActivity.java
+++ app/src/main/java/com/example/fitapp/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.fitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.fitapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // :)
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.workout) {
+                replaceFragment(new WorkoutFragment());
+            } else if (itemId == R.id.settings) {
+                replaceFragment(new SettingFragment());
+            } else if (itemId == R.id.about) {
+                replaceFragment(new AboutFragment());
+            }
+
+            return true;
+
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
